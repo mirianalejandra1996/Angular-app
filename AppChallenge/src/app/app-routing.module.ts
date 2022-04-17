6794f2290872c18e223c56a4f12491fa585fc972
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2022-04-17 00:39:23 -0500

    create router

---
+++ AppChallenge/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router'
+import { CustomersListComponent } from './modules/customer/customers-list/customers-list.component';
+import { CreateCustomerComponent } from './modules/customer/create-customer/create-customer.component';
+import { LoginComponent } from './auth/login/login.component';
+
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'list-customers', component: CustomersListComponent},
+  {path: 'create-customer', component: CreateCustomerComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full' }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes),
+  ],
+})
+export class AppRoutingModule { }
--- AppChallenge/src/app/app.module.ts
+++ AppChallenge/src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CustomersListComponent } from './components/customers-list/customers-list.component';
-import { CreateCustomerComponent } from './components/create-customer/create-customer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { CustomersListComponent } from './modules/customer/customers-list/customers-list.component';
+import { CreateCustomerComponent } from './modules/customer/create-customer/create-customer.component';
+import { NavbarComponent } from './modules/customer/components/navbar/navbar.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { LoginComponent } from './auth/login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomersListComponent,
     CreateCustomerComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
