e4e21942e9e4c157da6102664ce5fe1210780ef2
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2022-04-18 00:38:06 -0500

    deploy wip

--- AppChallenge/src/app/services/customer.service.ts
+++ AppChallenge/src/app/services/customer.service.ts
@@ -10,19 +10,23 @@ export class CustomerService {
   constructor(private http: HttpClient) { } //We have nothing to do here because it's just for making injection of the http request
 
   getCustomers(): Observable<any[]> {
-    return this.http.get<any[]>("/customers/customers/listclientes")
+    // return this.http.get<any[]>("/customers/customers/listclientes")
+    return this.http.get<any[]>("/customers/listclientes")
   }
   
   addCustomer(customer: any): Observable<any>{
-    return this.http.post<any>("/customers/customers/creacliente", customer);
+    // return this.http.post<any>("/customers/customers/creacliente", customer);
+    return this.http.post<any>("/customers/creacliente", customer);
   }
 
   getKpi(): Observable<any>{
-      return this.http.get<any>("/customers/kpideclientes")
+      // return this.http.get<any>("/customers/kpideclientes")
+      return this.http.get<any>("/kpideclientes")
   }
 
   getCustomerById(id: string): Observable<any> {
-    return this.http.get<any>(`/customers/customers/getById/${id}`)
+    return this.http.get<any>(`/customers/getById/${id}`)
+    // return this.http.get<any>(`/customers/customers/getById/${id}`)
   }
   // updateCustomerById(): Observable<any> {
 
